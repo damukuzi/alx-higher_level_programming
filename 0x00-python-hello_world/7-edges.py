a2fad36e5d6436bc1372674e418cfd0d6cfa3d7e
damukuzi <damukuzi@gmail.com>
2022-06-27 12:13:22 -0700

    adding changes

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -3,7 +3,7 @@ word = "Holberton"
 three = word[:3]
 two = word[-2:]
 middle = word[1:-1]
-print(f"First 3 letters: {three}")
-print(f"Last 2 letters: {two}")
-print(f"Middle word: {middle}")
+print(f"First 3 letters: {word[:3]}")
+print(f"Last 2 letters: {word[-2:]}")
+print(f"Middle word: {word[1:-1]}")
 
