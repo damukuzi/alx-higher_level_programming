fdaf68e9b9fa62406dba348d114f7754c99c3602
damukuzi <damukuzi@gmail.com>
2022-06-28 19:00:42 +0300

    adding changes

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
-print((i for i in list(map(chr, range(97, 123)))), end=" ")
+for i in range(ord('a'), ord('z') + 1):
+    if chr(i) == 'e' or chr(i) == 'q':
+        continue
+    else:
+        print("{:s}".format(chr(i)), end="")
